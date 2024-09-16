2bb292df87f848b8ea98ebf86b94873058ffc1d1
Marcos Colella <colella.dev@gmail.com>
2024-09-16 18:27:09 -0300

    Levando as estrelas para o canvas

--- src/components/Stars/styles.ts
+++ src/components/Stars/styles.ts
@@ -1,8 +1,15 @@
 import { StyleSheet } from "react-native";
 
+
 export const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
     alignItems: 'center'
+  },
+  canvas: {
+    width: 257,
+    height: 249,
+    position: 'absolute',
+    zIndex: 1
   }
 })
