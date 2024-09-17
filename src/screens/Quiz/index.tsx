28c53ec0a9416bc33156b67e5f2f71a553479dc7
Marcos Colella <colella.dev@gmail.com>
2024-09-17 08:26:36 -0300

    Tocando som

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { Alert, Text, View } from 'react-native';
 import { OverlayFeedback } from '../../components/OverlayFeedback';
 
+import { Audio } from 'expo-av';
+
 import { useNavigation, useRoute } from '@react-navigation/native';
 import Animated, { 
   useAnimatedStyle, 
@@ -55,6 +57,15 @@ export function Quiz() {
   const route = useRoute();
   const { id } = route.params as Params;
 
+  async function playSound(isCorrect: boolean) {
+    const file = isCorrect ? require('../../assets/correct.mp3') : require('../../assets/wrong.mp3');
+
+    const { sound } = await Audio.Sound.createAsync(file, { shouldPlay: true })
+
+    await sound.setPositionAsync(0);
+    await sound.playAsync();
+  }
+
   function handleSkipConfirm() {
     Alert.alert('Pular', 'Deseja realmente pular a questão?', [
       { text: 'Sim', onPress: () => handleNextQuestion() },
@@ -91,10 +102,13 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      await 
+      playSound(true)
       setStatusReply(1);
       setPoints(prevState => prevState + 1);
       handleNextQuestion();
     } else {
+      playSound(false)
       setStatusReply(2);
       shakeAnimation();
     }
